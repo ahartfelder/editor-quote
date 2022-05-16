22d9dca008ce718618a065c7968bf4328e729bd6
ahartfelder <ahartfelder@gmail.com>
2022-05-16 19:51:21 -0300

    line item dates - show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :quotes
   root to: "pages#home"
+  devise_for :users
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
