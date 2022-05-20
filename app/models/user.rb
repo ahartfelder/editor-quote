6e21c7d39603029e53991331ed352a1cbe264da2
ahartfelder <ahartfelder@gmail.com>
2022-05-20 11:41:02 -0300

    devise style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :company
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :validatable
 
   def name
     email.split("@").first.capitalize
